7fedca76e3194a6dc35c71f13e4fc69c75cfb948
Roshan <nahsorad@gmail.com>
2020-05-03 21:36:01 +0545

    Return promises from DockerApp functions

--- docker/app.js
+++ docker/app.js
@@ -2,57 +2,69 @@ const { exec, spawnSync } = require('child_process');
 
 class DockerApp {
     buildNodeImage = () => {
-        console.log('Building a Node.js image ... ');
-        exec('docker build -t img_node .', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error during Node.js image build: ${error}`);
-                return;
-            }
-            if (stderr) {
-                console.error(`stderr during Node.js image build: ${stderr}`);
-                return;
-            }
-            stdout ? console.error(`stdout during Node.js image build: ${stdout}`)
-                : console.log('Node.js image built.') ;
-            // console.log('creating a docker container ...');
-            // setTimeout(createContainer, 100);
+        return new Promise((resolve, reject) => {
+            console.log('Building a Node.js image ... ');
+            exec('docker build -t img_node .', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error during Node.js image build: ${error}`);
+                    reject(error);
+                }
+                if (stderr) {
+                    console.error(`stderr during Node.js image build: ${stderr}`);
+                    resolve({ success: true, stderr});
+                }
+                stdout ? console.error(`stdout during Node.js image build: ${stdout}`)
+                    : console.log('Node.js image built.');
+                resolve({ success: true, stdout });
+                // console.log('creating a docker container ...');
+                // setTimeout(createContainer, 100);
+            });
         });
     }
     
     createNodeContainer = () => {
-        // remove any preexisting container
-        exec('docker container rm cont_node --force', (error, stdout, stderr) => {
-            exec('docker container create -it --name cont_node img_node', (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`Error during Node.js container creation: ${error}`);
-                    return;
-                }
-                if (stderr) {
-                    console.error(`stderr during Node.js container creation: ${stderr}`);
-                    return;
-                }
-                stdout ? console.error(`stdout during Node.js container creation: ${stdout}`)
-                    : console.log('Node.js container created.') ;
-                // console.log('starting a docker container ...');
-                // setTimeout(startContainer, 100);
-            });
-        });	
+        return new Promise((resolve, reject) => {
+            console.log('Removing any prexisting Node.js container ... ');
+            // remove any preexisting container
+            exec('docker container rm cont_node --force', (error, stdout, stderr) => {
+                console.log('Creating a Node.js container ... ');
+                exec('docker container create -it --name cont_node img_node', (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`Error during Node.js container creation: ${error}`);
+                        reject(error);
+                    }
+                    if (stderr) {
+                        console.error(`stderr during Node.js container creation: ${stderr}`);
+                        resolve({ success: true, stderr});
+                    }
+                    stdout ? console.error(`stdout during Node.js container creation: ${stdout}`)
+                        : console.log('Node.js container created.');
+                    resolve({ success: true, stdout });
+                    // console.log('starting a docker container ...');
+                    // setTimeout(startContainer, 100);
+                });
+            });	
+        });
     }
     
     startNodeContainer = () => {
-        exec('docker container start cont_node', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error during Node.js container start: ${error}`);
-                return;
-            }
-            if (stderr) {
-                console.error(`stderr during Node.js container start: ${stderr}`);
-                return;
-            }
-            stdout ? console.error(`stdout during Node.js container start: ${stdout}`)
-                : console.log('Node.js container started.') ;
-            // console.log('executing ...');
-            // setTimeout(execInContainer, 100);
+        return new Promise((resolve, reject) => {
+            console.log('Building a Node.js image ... ');
+            exec('docker container start cont_node', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error during Node.js container start: ${error}`);
+                    reject(error);
+                }
+                if (stderr) {
+                    console.error(`stderr during Node.js container start: ${stderr}`);
+                    resolve({ success: true, stderr});
+                }
+                stdout ? console.error(`stdout during Node.js container start: ${stdout}`)
+                    : console.log('Node.js container started.');
+                resolve({ success: true, stdout });
+                // console.log('executing ...');
+                // setTimeout(execInContainer, 100);
+            });
         });
     }
     
@@ -67,49 +79,49 @@ class DockerApp {
         }
     }
     
-    runContainer = () => {
-        // remove container if exists
-        exec('docker container rm cont_node', (error, stdout, stderr) => {
-            const child = spawnSync('docker',
-                ['container', 'run', '-it', '--name', 'cont_node', 'img_node'], {
-                    stdio: 'inherit'
-            });
-        });	
-    }
+    // runContainer = () => {
+    //     // remove container if exists
+    //     exec('docker container rm cont_node', (error, stdout, stderr) => {
+    //         const child = spawnSync('docker',
+    //             ['container', 'run', '-it', '--name', 'cont_node', 'img_node'], {
+    //                 stdio: 'inherit'
+    //         });
+    //     });	
+    // }
     
-    attachContainer = () => {
-        const child_process = require('child_process');
-        child_process.spawnSync('docker', [ 'run', '--rm', '-ti', 'hello-world' ], {
-        stdio: 'inherit'
-    });
-        const child = spawn('docker', ['container', 'run', 'cont_node']);
+    // attachContainer = () => {
+    //     const child_process = require('child_process');
+    //     child_process.spawnSync('docker', [ 'run', '--rm', '-ti', 'hello-world' ], {
+    //     stdio: 'inherit'
+    // });
+    //     const child = spawn('docker', ['container', 'run', 'cont_node']);
     
-        child.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-        });
+    //     child.stderr.on('data', (data) => {
+    //         console.error(`stderr: ${data}`);
+    //     });
     
-        child.stdout.on('data', (data) => {
-            console.log('attached a container');
-            console.log(data.toString());
+    //     child.stdout.on('data', (data) => {
+    //         console.log('attached a container');
+    //         console.log(data.toString());
             
-        exec('docker container attach cont_node', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                return;
-            }
-            console.log('attached a container');
-            exec(`console.log("Hello World!");`, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    return;
-                }
-                console.log(`final stdout: ${stdout}`);
-                console.error(`stderr: ${stderr}`);
+    //     exec('docker container attach cont_node', (error, stdout, stderr) => {
+    //         if (error) {
+    //             console.error(`exec error: ${error}`);
+    //             return;
+    //         }
+    //         console.log('attached a container');
+    //         exec(`console.log("Hello World!");`, (error, stdout, stderr) => {
+    //             if (error) {
+    //                 console.error(`exec error: ${error}`);
+    //                 return;
+    //             }
+    //             console.log(`final stdout: ${stdout}`);
+    //             console.error(`stderr: ${stderr}`);
     
-            });
-        });
-        });
-    }
+    //         });
+    //     });
+    //     });
+    // }
 }
 
 module.exports = DockerApp;
