0a60446f987fa0445947ce03a534ccc03018a8b5
Roshan <nahsorad@gmail.com>
2020-05-03 20:03:14 +0545

    Handle POST request at /execute

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const { dockerApp } = require('./docker/app.js')
+
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-    console.log("Request received at /");
+    console.log("GET request received at /");
+    res.send("Hello World!");
+});
+
+app.post('/execute', (req, res) => {
+    console.log("POST request received at /execute");
     res.send("Hello World!");
 });
 
