0f36f9b899b62fde6b2594537deeace1b08dbfc1
Roshan <nahsorad@gmail.com>
2020-05-04 20:25:47 +0545

    Read output from .output file

--- file/index.js
+++ file/index.js
@@ -2,11 +2,19 @@ const fs = require('fs');
 
 module.exports.updateCodeInFile = (code) => {
     let filePath = __dirname + '/submission.js';
-    console.log(filePath);
     try {
         fs.writeFileSync(filePath, code);
         console.log(`Submitted code written to file: ${code} > ${filePath}`);
     } catch (err) {
         return console.error(`Error during writing code to file: ${err}`);
+    }
+}
+
+module.exports.readOuput = () => {
+    let filePath = __dirname + '/.output';
+    try {
+        return fs.readFileSync(filePath);
+    } catch (err) {
+        return console.error(`Error during reading output from file: ${err}`);
     }
 }
