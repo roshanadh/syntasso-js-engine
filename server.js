047f680b7d8daa8d886d65e2fb5e3f94f5594812
Roshan <nahsorad@gmail.com>
2020-05-30 16:56:49 +0545

    Import createSocket function from socket.js

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const createSocket = require('./socket/socket.js');
 const { PORT } = require('./utils.js');
 
 const app = express();
+app.use(cors());
+
 const router = require('./routes/router.js');
 
 app.set('json spaces', 2);
 
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -18,5 +20,7 @@ app.use(router);
 const server = app.listen(PORT, () => {
     console.log(`Syntasso JS Engine server listening on port ${PORT}...`);
 });
+
+createSocket(server);
 
 module.exports = server;
