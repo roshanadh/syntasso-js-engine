af5e38e2436003bdf46775a4774bb67134229a8f
Roshan <nahsorad@gmail.com>
2020-05-31 16:25:42 +0545

    Export SECRET_SESSION_KEY from utils.js

--- utils.js
+++ utils.js
@@ -5,4 +5,5 @@ if (!process.env) throw new Error('Environment variable(s) not set.');
 module.exports = {
     PORT: process.env.PORT || 8080,
     SECRET_DIVIDER_TOKEN: process.env.SECRET_DIVIDER_TOKEN,
+    SECRET_SESSION_KEY: process.env.SECRET_SESSION_KEY,
 }
