ebe14aa9f48a8a8e97d8145048b8b764551276e3
Roshan <nahsorad@gmail.com>
2020-05-31 12:51:58 +0545

    Update comments and indentation in executionController

--- controllers/executionController.js
+++ controllers/executionController.js
@@ -2,23 +2,26 @@ const Handler = require('./DockerConfigHandler.js');
 const { updateCodeInFile } = require('../file/index.js');
 
 const handler = new Handler();
-executionController = (req, res) => {
-    console.log("POST request received at /execute");
+module.exports = executionController = (req, res) => {
     /*
     * All req.body params =>
-    * 1. req.body.code: String => contains JavaScript code to be compiled and executed
-    * 2. req.body.dockerConfig: Integer => 
+    * 1. req.body.socketId: String => contains socket ID of the connected client
+    * 2. req.body.code: String => contains JavaScript code to be compiled and executed
+    * 3. req.body.dockerConfig: Stringified Integer => 
     * ... a) if 0, docker environment (image build and container create) should be setup ...
     * ... ... before code execution
     * ... b) if 1, docker container should be started before code execution, no need to ....
     * ... ... build an image or to create a container
     * ... c) if 2, just execute the code, no need to create and/or start the container
+    * 
+    * executionController middleware deals with the code and dockerConfig parameters
     */
-    if(!req.body.code) {
+
+    if (!req.body.code) {
         res.status(400).json({ error: "Bad Request: No Code Provided!" });
         throw new Error("Bad Request Error at /execute POST. No Code Provided!");
     }
-    if(!req.body.dockerConfig) {
+    if (!req.body.dockerConfig) {
         res.status(400).json({ error: "Bad Request: No Docker Configuration Instruction Provided!" });
         throw new Error("Bad Request Error at /execute POST. No Docker Configuration Instruction Provided!");
     }
