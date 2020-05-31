e13b27362d84142fcb9f9f0b2fa02dd0a323227c
Roshan <nahsorad@gmail.com>
2020-05-31 12:52:19 +0545

    Add socketController middleware in POST /execute

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const executionController = require('../controllers/executionController.js')
+const socketController = require('../controllers/socketController.js');
+const executionController = require('../controllers/executionController.js');
 
 router.get('/', (req, res) => {
     console.log("GET request received at /");
     res.send("Hello World!");
 });
 
-router.post('/execute', executionController);
+router.post('/execute', socketController, executionController);
 
 module.exports = router;
