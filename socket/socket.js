5e825020956e325a1f1bc4517c9fbb4557ca5f7e
Roshan <nahsorad@gmail.com>
2020-05-31 12:55:53 +0545

    Export Socket class from socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -1,14 +1,17 @@
 const socket = require('socket.io');
 
-module.exports = createSocket = server => {
-    const io = socket(server);
-
-    io.on('connection', socket => {
-        console.log(`\nCONNECTION: New socket connection with id ${socket.id}\n`);
-    
-        socket.on('disconnect', reason => {
-            console.log(`\nDISCONNECT: Socket disconnected with id ${socket.id}`);
-            console.log(`REASON: ${reason}\n`);
+class Socket {
+    constructor(server) {
+        this.instance = socket(server);
+        this.instance.on('connection', socket => {
+            console.log(`\nCONNECTION: New socket connection with id ${socket.id}\n`);
+            
+            socket.on('disconnect', reason => {
+                console.log(`\nDISCONNECT: Socket disconnected with id ${socket.id}`);
+                console.log(`REASON: ${reason}\n`);
+            });
         });
-    });
+    }
 }
+
+module.exports = Socket;
