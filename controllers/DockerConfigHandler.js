1874270270a6cdf3a4188e951a64574dfecb067a
Roshan <nahsorad@gmail.com>
2020-06-01 20:03:39 +0545

    Respond if requested cont doesn't exist or is idle

--- controllers/DockerConfigHandler.js
+++ controllers/DockerConfigHandler.js
@@ -74,9 +74,16 @@ class DockerConfigHandler {
             : console.log('Node.js container started.');
 
         } catch (err) {
-            // handle promise rejection
-            res.status(503).send(`Service currently unavailable due to server conditions.`);
-            throw new Error(`Error in dockerApp.startNodeContainer(): ${err}`);
+            // handle promise rejection of dockerApp.startNodeContainer()
+            if (err.errorType === 'container-not-created-beforehand') {
+                res.status(503).json({
+                    error: "The container has not been created on the server. Request again with dockerConfig 0."
+                });
+                return console.error('Requested container could not be started because it has not been created yet.');
+            } else {
+                res.status(503).send(`Service currently unavailable due to server conditions.`);
+                throw new Error(`Error in dockerApp.startNodeContainer(): ${err}`);
+            }
         }
 
         let { error, execTime } = dockerApp.execInNodeContainer(req.session);
@@ -97,20 +104,15 @@ class DockerConfigHandler {
     }
 
     handleConfigTwo = (req, res) => {
-        let { error, execTime } = dockerApp.execInNodeContainer(req.session);
+        let { error, errorType, execTime } = dockerApp.execInNodeContainer(req.session);
         if (error) {
-            console.error(`Error in dockerApp.execInNodeContainer(): ${error}`);
-            /*
-             * Check if the error message is for an idle container
-            */
-            try {
-                if (error.slice(-15).trim() === "is not running")
-                    res.status(503).json({
-                        error: "The container is not currently running on the server. Request again with dockerConfig 0 or 1."
-                    });
-                else res.status(503).send(`Service currently unavailable due to server conditions.`);
-            } catch (err) {
-                console.error(`Error during slicing the error message from dockerApp.execInNodeContainer(): ${err}`);
+            if (errorType === 'container-not-started-beforehand') {
+                res.status(503).json({
+                    error: "The container is not currently running on the server. Request again with dockerConfig 0 or 1."
+                });
+                return console.error('Requested container is not currently running on the server.');
+            } else {
+                console.error(`Error in dockerApp.execInNodeContainer(): ${error}`);
                 res.status(503).send(`Service currently unavailable due to server conditions.`);
             }
         } else {
