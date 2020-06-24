379c5475a937aabd9eec98fdd0cd2a6629cd4b92
Roshan <nahsorad@gmail.com>
2020-06-24 18:16:31 +0545

    Update cleanup test suite to check for client files

--- test/4-cleanup.test.js
+++ test/4-cleanup.test.js
@@ -1,11 +1,14 @@
 const { mocha, chai, should, expect, path, fs, execSync } = require("./test-config.js");
 
 describe("4. Clean up files after socket disconnect", () => {
-	let socket, socketId;
+	let socket, socketId, tempFilesPath, outputFilePath;
 	before(async () => {
 		const { removeConnection } = require("./test-config.js");
-		socket= await removeConnection();
-		socketId = socket.id;
+		let removedConnection = await removeConnection();
+		socket = removedConnection.socket;
+		socketId = removedConnection.socketId;
+		tempFilesPath = path.resolve("client-files", socketId);
+		outputFilePath = path.resolve("client-files", "outputs", `${socketId}.txt`);
 	});
 
 	it("should disconnect from socket", done => {
@@ -20,38 +23,12 @@ describe("4. Clean up files after socket disconnect", () => {
 		expect(searchContainerOutput.toString().trim()).to.be.empty;
 		done();
 	});
-	it("should remove the JavaScript file if it was created", done => {
-		const filePath = path.resolve("client-files", "submissions", `${socketId}.js`);
-		expect(fs.existsSync(filePath)).to.be.false;
+	it("should remove the temporary client files if they were created", done => {
+		expect(fs.existsSync(tempFilesPath)).to.be.false;
 		done();
 	});
 	it("should remove the output file if it was created", done => {
-		const filePath = path.resolve("client-files", "outputs", `${socketId}.txt`);
-		expect(fs.existsSync(filePath)).to.be.false;
-		done();
-	});
-	it("should remove any sample input files if they exist", done => {
-		const filePath = path.resolve("client-files", "tests", "sampleInputs");
-		let count = 0;
-		fs.readdirSync(filePath)
-			.forEach(sampleInputFile => {
-				let fileNames = sampleInputFile.split("-");
-				let parsedSocketId = `${fileNames[0]}-${fileNames[1]}`;
-				if (parsedSocketId === socketId) count++;
-			});
-		expect(count).to.equal(0);
-		done();
-	});
-	it("should remove any expected output files if they exist", done => {
-		const filePath = path.resolve("client-files", "tests", "expectedOutputs");
-		let count = 0;
-		fs.readdirSync(filePath)
-			.forEach(expectedOutputFile => {
-				let fileNames = expectedOutputFile.split("-");
-				let parsedSocketId = `${fileNames[0]}-${fileNames[1]}`;
-				if (parsedSocketId === socketId) count++;
-			});
-		expect(count).to.equal(0);
+		expect(fs.existsSync(outputFilePath)).to.be.false;
 		done();
 	});
 });
