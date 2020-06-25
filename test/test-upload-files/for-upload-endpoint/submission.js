e53ee4be8c76d908a574e8075d06326bbcc069c2
Roshan <nahsorad@gmail.com>
2020-06-25 18:01:47 +0545

    Update submission.js test file to accept stdin

--- test/test-upload-files/for-upload-endpoint/submission.js
+++ test/test-upload-files/for-upload-endpoint/submission.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+"use strict";
+
+process.stdin.setEncoding("utf-8");
+let inputString = "";
+
+process.stdin.on("data", data => {
+	if (data.toString().trim() !== "") inputString += data;
+});
+
+process.stdin.on("end", () => {
+	if (inputString.trim() !== "") inputString = inputString.toString();
+	main();
+});
+
+const main = () => {
+	console.log(
+		inputString === ""
+			? "Hello World!"
+			: inputString
+	);
+}
