850e48e46b92cc8d92bf681230a537cb52535886
Roshan <nahsorad@gmail.com>
2020-06-27 19:11:31 +0545

    Parse rawInputString in submission.js

--- test/test-upload-files/for-upload-endpoint/submission.js
+++ test/test-upload-files/for-upload-endpoint/submission.js
@@ -1,21 +1,30 @@
 "use strict";
 
 process.stdin.setEncoding("utf-8");
-let inputString = "";
+let rawInputString = "",
+	jsonInputString = {},
+	sampleInputId = "",
+	sampleInputFileContents = [];
 
 process.stdin.on("data", data => {
-	if (data.toString().trim() !== "") inputString += data;
+	if (data.toString().trim() !== "") rawInputString += data;
 });
 
 process.stdin.on("end", () => {
-	if (inputString.trim() !== "") inputString = inputString.toString();
+	if (rawInputString.trim() !== "") {
+		jsonInputString = JSON.parse(rawInputString);
+		sampleInputId = jsonInputString.sampleInputId;
+		sampleInputFileContents = JSON.parse(jsonInputString.fileContents);
+
+		// remove any empty elements from the array sampleInputFileContents
+		sampleInputFileContents = sampleInputFileContents.filter(el => el.trim() !== "");
+	}
 	main();
 });
 
 const main = () => {
-	console.log(
-		inputString === ""
-			? "Hello World!"
-			: inputString
-	);
+	if (jsonInputString === {})
+		process.stdout.write(Buffer.from(JSON.stringify("No sample input was passed")));
+	else
+		process.stdout.write(Buffer.from("Hello World!\n"));
 }
