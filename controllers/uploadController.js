d55246a40ecd2d3babe086db2b38c36ea6c5c28d
Roshan <nahsorad@gmail.com>
2020-07-02 20:53:46 +0545

    Use .then() with initDirectories

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -88,92 +88,93 @@ let fileUpload = upload.fields([
 
 module.exports = uploadController = (req, res) => {
 	console.log("POST request received at /upload");
-	initDirectories();
-	sampleInputFileNameIndex = 0; expectedOutputFileNameIndex = 0;
-	/*
-	* All possible req.body params =>
-	* 1. req.body.socketId: String => contains socket ID of the connected client
-	* 
-	* 2. req.body.dockerConfig: Stringified Integer => 
-	* ... a) if 0, docker environment (image build and container create) should be setup ...
-	* ... ... before code execution
-	* ... b) if 1, docker container should be started before code execution, no need to ....
-	* ... ... build an image or to create a container
-	* ... c) if 2, just execute the code, no need to create and/or start the container
-	* 
-	* uploadController middleware deals with the dockerConfig request body parameter ...
-	* ... and the file included in the request
-	*/
-	
-	fileUpload(req, res, async (err) => {
-		try {
-			await socketValidator(req, res);
-			if (err instanceof multer.MulterError) {
-				// A Multer error occurred during uploading
-				res.status(503).json({
-					error: 'An error occurred while uploading the submitted JavaScript file!',
-					message: err.message,
-				});
-				console.error(`A Multer error occurred at uploadController while uploading:\n${err}`);
-			} else if (err) {
-				// An error occurred during uploading
-				res.status(503).json({
-					error: 'An error occurred while uploading the submitted JavaScript file!',
-					message: err.message,
-				});
-				console.error(`An error occurred at uploadController while uploading:\n${err}`);
-			} else {
-				if (!req.files["submission"]) {
-					res.status(400).json({ error: "Bad Request: No JavaScript File Provided!" });
-					return console.error('Bad Request Error at /upload POST. No JavaScript File Provided!');
-				}
-				if (!req.body.dockerConfig) {
-					res.status(400).json({ error: "Bad Request: No Docker Configuration Instruction Provided!" });
-					return console.error("Bad Request Error at /upload POST. No Docker Configuration Instruction Provided!");
-				}
-				// add secret divider token to the user-submitted file
-				await addDividerToken(req.session.socketId);
-				if (isNaN(req.body.dockerConfig))
-					throw new ErrorWithStatus(
-						400,
-						"Bad Request: dockerConfig Value Is Not A Number!"
-					);
-				let dockerConfig = parseInt(req.body.dockerConfig);
-				
-				switch (dockerConfig) {
-					case 0:
-						handleConfigZero(req, res);
-						break;
-					case 1:
-						handleConfigOne(req, res);
-						break;
-					case 2:
-						handleConfigTwo(req, res);
-						break;
-					default:
+	initDirectories().then(() => {
+		sampleInputFileNameIndex = 0; expectedOutputFileNameIndex = 0;
+		/*
+		* All possible req.body params =>
+		* 1. req.body.socketId: String => contains socket ID of the connected client
+		* 
+		* 2. req.body.dockerConfig: Stringified Integer => 
+		* ... a) if 0, docker environment (image build and container create) should be setup ...
+		* ... ... before code execution
+		* ... b) if 1, docker container should be started before code execution, no need to ....
+		* ... ... build an image or to create a container
+		* ... c) if 2, just execute the code, no need to create and/or start the container
+		* 
+		* uploadController middleware deals with the dockerConfig request body parameter ...
+		* ... and the file included in the request
+		*/
+
+		fileUpload(req, res, async (err) => {
+			try {
+				await socketValidator(req, res);
+				if (err instanceof multer.MulterError) {
+					// A Multer error occurred during uploading
+					res.status(503).json({
+						error: 'An error occurred while uploading the submitted JavaScript file!',
+						message: err.message,
+					});
+					console.error(`A Multer error occurred at uploadController while uploading:\n${err}`);
+				} else if (err) {
+					// An error occurred during uploading
+					res.status(503).json({
+						error: 'An error occurred while uploading the submitted JavaScript file!',
+						message: err.message,
+					});
+					console.error(`An error occurred at uploadController while uploading:\n${err}`);
+				} else {
+					if (!req.files["submission"]) {
+						res.status(400).json({ error: "Bad Request: No JavaScript File Provided!" });
+						return console.error('Bad Request Error at /upload POST. No JavaScript File Provided!');
+					}
+					if (!req.body.dockerConfig) {
+						res.status(400).json({ error: "Bad Request: No Docker Configuration Instruction Provided!" });
+						return console.error("Bad Request Error at /upload POST. No Docker Configuration Instruction Provided!");
+					}
+					// add secret divider token to the user-submitted file
+					await addDividerToken(req.session.socketId);
+					if (isNaN(req.body.dockerConfig))
 						throw new ErrorWithStatus(
 							400,
-							"Bad Request: dockerConfig Value Is Not A Valid Number!"
+							"Bad Request: dockerConfig Value Is Not A Number!"
 						);
+					let dockerConfig = parseInt(req.body.dockerConfig);
+
+					switch (dockerConfig) {
+						case 0:
+							handleConfigZero(req, res);
+							break;
+						case 1:
+							handleConfigOne(req, res);
+							break;
+						case 2:
+							handleConfigTwo(req, res);
+							break;
+						default:
+							throw new ErrorWithStatus(
+								400,
+								"Bad Request: dockerConfig Value Is Not A Valid Number!"
+							);
+					}
 				}
-			}
-		} catch (err) {
-			// if the error object 'err' contains a status code, ...
-			// ... it was thrown using custom ErrorWithStatus class
-			if (err.status)
-				return res.status(err.status).json({
+			} catch (err) {
+				// if the error object 'err' contains a status code, ...
+				// ... it was thrown using custom ErrorWithStatus class
+				if (err.status)
+					return res.status(err.status).json({
+						error: err.message,
+					});
+				// otherwise, a different error occurred and should be ...
+				// ... logged to console
+				console.error({
 					error: err.message,
+					stack: err.stack
 				});
-			// otherwise, a different error occurred and should be ...
-			// ... logged to console
-			console.error({
-				error: err.message,
-				stack: err.stack
-			});
-			res.status(503).json({
-				error: 'Service is currently unavailable!',
-			});
-		}
+				res.status(503).json({
+					error: 'Service is currently unavailable!',
+				});
+			}
+		});
 	});
 }
 
