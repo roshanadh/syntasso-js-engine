ef98b7cbb0c9995c77e86a16098c60586b5f397e
Roshan <nahsorad@gmail.com>
2020-07-02 19:42:29 +0545

    Return promise from readOutput

--- filesystem/index.js
+++ filesystem/index.js
@@ -88,134 +88,145 @@ module.exports.readOutput = async (socketId) => {
 		socketId + ".txt"	
 	);
 	let fileContents = "";
-	try {
-	/*
-	 *  Reads output of the user-submitted code.
-	 *  Also reads any errors present in the form of the object:
-	 *  { errorName: '', errorMessage: '', errorStack: '' }
-	 *
-	 *  The output is written in output.txt along with the error object.
-	 *  To parse the error object safely from the output file, we substring ...
-	 *  ... the content of the file output.txt.
-	 *  To find the point of substring (i.e. the position of error object) ...
-	 *  ... we place a SECRET_DIVIDER_TOKEN in the output.txt file just before the ...
-	 *  ... error object and just after the output of the user-submitted code
-	 *
-	 *  We then search for the SECRET_DIVIDER_TOKEN in the fileContent string to get ...
-	 *  ... point of substring.
-	 *
-	 */
-		fileContents = await fs.readFileSync(filePath).toString("utf-8");
-		if (fileContents === "")
-		// the output file has not been populated
-			return {
-				sampleInputs: null,
-				testStatus: null,
-				expectedOutput: null,
-				observedOutput: null,
-				error: null,
-				errorInProcess: "The output file has not been populated"
-			}
-		// fetch fileContents.sampleInputs to get number of test cases
-		// and get observed output for each sampleInput
-		fileContents = JSON.parse(fileContents);
-		let sampleInputs = fileContents.sampleInputs;
-		let observedOutput = {},
-			response = {
-				sampleInputs,
-			},
-			error;
-		// else: the output file has been populated ...
-		// ... parse the output file to get output for each ...
-		// ... sample input
-
-		// if no sampleInput file was uploaded
-		if (sampleInputs === 0) {
-			observedOutput = fileContents.observedOutput;
-			const startIndex = observedOutput.search(SECRET_DIVIDER_TOKEN);
-
-			if (startIndex === -1) {
-				// no error was observed if SECRET_DIVIDER_TOKEN ...
-				// ... doesn't exist in observedOutput object
-				error = null;
-			} else {
-				// length of SECRET_DIVIDER_TOKEN is 10
-				error = observedOutput.substring(startIndex + 10).trim();
-				observedOutput = observedOutput.substring(0, startIndex);
-
-				error = JSON.parse(error);
-				// parse error line number and column number from errorStack
-				let stack = error.errorStack;
-				let index = stack.search(`/home/client-files/${socketId}/submission.js:`);
-
-				let str = stack.substring(index + `/home/client-files/${socketId}/submission.js:`.length);
-				let lineNumber = str.split(':')[0];
-				let columnNumber = str.split(':')[1].split(')')[0];
-
-				// delete errorStack property from error object to reorder its occurrence ...
-				// ... below lineNumber and columnNumber
-				delete error.errorStack;
-				error = {
-					...error,
-					lineNumber,
-					columnNumber,
-					errorStack: stack,
+	return new Promise((resolve, reject) => {
+		try {
+			/*
+			 *  Reads output of the user-submitted code.
+			 *  Also reads any errors present in the form of the object:
+			 *  { errorName: '', errorMessage: '', errorStack: '' }
+			 *
+			 *  The output is written in output.txt along with the error object.
+			 *  To parse the error object safely from the output file, we substring ...
+			 *  ... the content of the file output.txt.
+			 *  To find the point of substring (i.e. the position of error object) ...
+			 *  ... we place a SECRET_DIVIDER_TOKEN in the output.txt file just before the ...
+			 *  ... error object and just after the output of the user-submitted code
+			 *
+			 *  We then search for the SECRET_DIVIDER_TOKEN in the fileContent string to get ...
+			 *  ... point of substring.
+			 *
+			 */
+			fs.readFile(filePath, (err, data) => {
+				if (err) {
+					console.error(`Error during reading output from file: ${err.stack}`);
+					reject({ errorInProcess: err });
 				}
-			}
-			return {
-				sampleInputs: fileContents.sampleInputs,
-				expectedOutput: null,
-				observedOutput,
-				error
-			}
-		}
-		// else: at least one sample input file has been uploaded
-		for (let i = 0; i < sampleInputs; i++) {
-			// read observedOutput for each sampleInput
-			observedOutput = fileContents[`sampleInput${i}`].observedOutput;
+				fileContents = data;
+				if (fileContents.toString().trim() === "")
+					// the output file has not been populated
+					resolve({
+						sampleInputs: null,
+						testStatus: null,
+						expectedOutput: null,
+						observedOutput: null,
+						error: null,
+						errorInProcess: "The output file has not been populated"
+					});
+				// fetch fileContents.sampleInputs to get number of test cases
+				// and get observed output for each sampleInput
+				fileContents = JSON.parse(fileContents.toString());
+				
+				let sampleInputs = fileContents.sampleInputs;
+				let observedOutput = {},
+					response = {
+						sampleInputs,
+					},
+					error;
+				// else: the output file has been populated ...
+				// ... parse the output file to get output for each ...
+				// ... sample input
 
-			const startIndex = observedOutput.search(SECRET_DIVIDER_TOKEN);
-			if (startIndex === -1) {
-				// no error was observed if SECRET_DIVIDER_TOKEN ...
-				// ... doesn't exist in observedOutput object
-				error = null;
-			} else {
-				// length of SECRET_DIVIDER_TOKEN is 10
-				error = observedOutput.substring(startIndex + 10).trim();
-				observedOutput = observedOutput.substring(0, startIndex);
+				// if no sampleInput file was uploaded
+				if (sampleInputs === 0) {
+					observedOutput = fileContents.observedOutput;
+					const startIndex = observedOutput.search(SECRET_DIVIDER_TOKEN);
 
-				error = JSON.parse(error);
-				// parse error line number and column number from errorStack
-				let stack = error.errorStack;
-				let index = stack.search(`/home/client-files/${socketId}/submission.js:`);
+					if (startIndex === -1) {
+						// no error was observed if SECRET_DIVIDER_TOKEN ...
+						// ... doesn't exist in observedOutput object
+						error = null;
+					} else {
+						// length of SECRET_DIVIDER_TOKEN is 10
+						error = observedOutput.substring(startIndex + 10).trim();
+						observedOutput = observedOutput.substring(0, startIndex);
 
-				let str = stack.substring(index + `/home/client-files/${socketId}/submission.js:`.length);
-				let lineNumber = str.split(':')[0];
-				let columnNumber = str.split(':')[1].split(')')[0];
+						error = JSON.parse(error);
+						// parse error line number and column number from errorStack
+						let stack = error.errorStack;
+						let index = stack.search(`/home/client-files/${socketId}/submission.js:`);
 
-				// delete errorStack property from error object to reorder its occurrence ...
-				// ... below lineNumber and columnNumber
-				delete error.errorStack;
-				error = {
-					...error,
-					lineNumber,
-					columnNumber,
-					errorStack: stack,
+						let str = stack.substring(index + `/home/client-files/${socketId}/submission.js:`.length);
+						let lineNumber = str.split(':')[0];
+						let columnNumber = str.split(':')[1].split(')')[0];
+
+						// delete errorStack property from error object to reorder its occurrence ...
+						// ... below lineNumber and columnNumber
+						delete error.errorStack;
+						error = {
+							...error,
+							lineNumber,
+							columnNumber,
+							errorStack: stack,
+						}
+					}
+					resolve({
+						sampleInputs: fileContents.sampleInputs,
+						expectedOutput: null,
+						observedOutput,
+						error
+					});
 				}
-			}
-			response[`sampleInput${i}`] = {
-				testStatus: fileContents[`sampleInput${i}`].testStatus,
-				sampleInput: fileContents[`sampleInput${i}`].sampleInput,
-				expectedOutput: fileContents[`sampleInput${i}`].expectedOutput,
-				observedOutput,
-				error,
-				execTimeForProcess: fileContents[`sampleInput${i}`].execTimeForProcess
-			}
+				// else: at least one sample input file has been uploaded
+				for (let i = 0; i < sampleInputs; i++) {
+					// read observedOutput for each sampleInput
+					observedOutput = fileContents[`sampleInput${i}`].observedOutput;
+
+					const startIndex = observedOutput.search(SECRET_DIVIDER_TOKEN);
+					if (startIndex === -1) {
+						// no error was observed if SECRET_DIVIDER_TOKEN ...
+						// ... doesn't exist in observedOutput object
+						error = null;
+					} else {
+						// length of SECRET_DIVIDER_TOKEN is 10
+						error = observedOutput.substring(startIndex + 10).trim();
+						observedOutput = observedOutput.substring(0, startIndex);
+
+						error = JSON.parse(error);
+						// parse error line number and column number from errorStack
+						let stack = error.errorStack;
+						let index = stack.search(`/home/client-files/${socketId}/submission.js:`);
+
+						let str = stack.substring(index + `/home/client-files/${socketId}/submission.js:`.length);
+						let lineNumber = str.split(':')[0];
+						let columnNumber = str.split(':')[1].split(')')[0];
+
+						// delete errorStack property from error object to reorder its occurrence ...
+						// ... below lineNumber and columnNumber
+						delete error.errorStack;
+						error = {
+							...error,
+							lineNumber,
+							columnNumber,
+							errorStack: stack,
+						}
+					}
+					response[`sampleInput${i}`] = {
+						testStatus: fileContents[`sampleInput${i}`].testStatus,
+						sampleInput: fileContents[`sampleInput${i}`].sampleInput,
+						expectedOutput: fileContents[`sampleInput${i}`].expectedOutput,
+						observedOutput,
+						error,
+						execTimeForProcess: fileContents[`sampleInput${i}`].execTimeForProcess
+					}
+				}
+				resolve({ ...response });
+			});
+
+		} catch (err) {
+			console.error(`Error during reading output from file: ${err.stack}`);
+			reject({ errorInProcess: err });
 		}
-		return response;
-	} catch (err) {
-		return console.error(`Error during reading output from file: ${err.stack}`);
-	}
+	});
 }
 
 module.exports.removeTempFiles = (socketId) => {
