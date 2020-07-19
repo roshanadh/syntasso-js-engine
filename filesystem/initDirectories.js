6f23b6fb6a4bd70c7063c8488b184ab17a5e61b9
Roshan <nahsorad@gmail.com>
2020-07-19 19:45:46 +0545

    Check for EEXIST code when creating outputs dir

--- filesystem/initDirectories.js
+++ filesystem/initDirectories.js
@@ -9,22 +9,11 @@ module.exports = () => {
 			"client-files",
 			"outputs"
 		);
-		fs.stat(outputsDirPath, (err, stats) => {
-			if (err) {
-				if (err.message.includes("ENOENT: no such file or directory")) {
-					fs.mkdir(outputsDirPath, (err) => {
-						if (err) {
-							console.error(`Error while creating outputs directory: ${err.stack}`);
-							reject(false);
-							throw err;
-						}
-						resolve(true);
-					});
-				} else {
-					console.error(`Error while reading outputs directory: ${err.stack}`);
-					reject(false);
-					throw err;
-				}
+		fs.mkdir(outputsDirPath, (err) => {
+			if (err && !err.code === "EEXIST") {
+				console.error(`Error while creating outputs directory: ${err.stack}`);
+				reject(false);
+				throw err;
 			}
 			resolve(true);
 		});
