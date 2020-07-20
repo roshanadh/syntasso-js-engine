1450d949375333bba8558c5b76a7ee9493d9bf85
Roshan <nahsorad@gmail.com>
2020-07-20 23:09:25 +0545

    Remove try...catch wrapper for submitted code

--- filesystem/updateCodeInFile.js
+++ filesystem/updateCodeInFile.js
@@ -12,14 +12,7 @@ module.exports = (socketId, code) => {
 			socketId,
 			"submission.js"
 		);
-		// wrap user-submitted code inside a try-catch block
-		
-		// NOTE: DO NOT MODIFY THIS BLOCK AS IT AFFECTS THE ...
-		// ... LINE NUMBER AND COLUMN NUMBER FOR ANY ERROR INSTANCE
-		let finalCode =
-		`"use strict";try{\n${code}\n}catch(err){console.log('${SECRET_DIVIDER_TOKEN}');
-		console.log(JSON.stringify({ errorName: err.name, errorMessage: err.message, errorStack: err.stack }));
-		}`;
+
 		/*
 		 * /client-files/${socketId} directory may not have been created if ...
 		 * ... no sampleInputs and expectedOutputs files have been uploaded ...
@@ -27,16 +20,17 @@ module.exports = (socketId, code) => {
 		 * ... of multer.diskStorage.
 		 * So, create the required directories if they do not exist yet.
 		*/
-		let basePath = path.resolve(__dirname, "..", "client-files", socketId);
-		// check if ${basePath} dir exits before creating it
 
+		let basePath = path.resolve(__dirname, "..", "client-files", socketId);
+		
+		// check if ${basePath} dir exits before creating it
 		fs.mkdir(basePath, { recursive:true }, (err) => {
 			if (err) {
 				console.error(`Error while creating client-files/${socketId}/ directory: ${err.stack}`);
 				reject(false);
 				throw err;
 			}
-			fs.writeFile(filePath, finalCode, (err) => {
+			fs.writeFile(filePath, code, (err) => {
 				if (err) {
 					console.error(`Error during writing code to file: ${err.stack}`);
 					reject(false);
