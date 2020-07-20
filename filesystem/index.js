68133ec2054587d785002edb4ad424002ec543d1
Roshan <nahsorad@gmail.com>
2020-07-21 00:17:13 +0545

    Remove unused import from addDividerToken

--- filesystem/index.js
+++ filesystem/index.js
@@ -1,7 +1,6 @@
 const initDirectories = require("./initDirectories.js");
 const generateTestFiles = require("./generateTestFiles.js");
 const generateSubmissionFile = require("./generateSubmissionFile.js");
-const addDividerToken = require("./addDividerToken.js");
 const readOutput = require("./readOutput.js");
 const cleanUpTempFiles = require("./cleanUpTempFiles.js");
 const removeTestCases = require("./removeTestCases.js");
@@ -10,7 +9,6 @@ module.exports = {
 	initDirectories,
 	generateTestFiles,
 	generateSubmissionFile,
-	addDividerToken,
 	readOutput,
 	cleanUpTempFiles,
 	removeTestCases,
