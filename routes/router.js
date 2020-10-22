95ab2a280c469e3bae274fb08032aa82cd52a914
Roshan <nahsorad@gmail.com>
2020-10-22 17:57:16 +0545

    Remove unused routes and controllers

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const executionController = require("../controllers/executionController.js");
-const uploadController = require("../controllers/uploadController.js");
 const submitController = require("../controllers/submitController.js");
 
 const { paramValidator, errorHandler } = require("../middlewares/index.js");
@@ -14,12 +12,6 @@ router.get("/", (req, res) => {
 	});
 });
 
-// POST with code snippet and optional ...
-// ... sample input and expected output files
-router.post("/execute", executionController);
-// POST with .js file and optional ...
-// ... sample input and expected output files
-router.post("/upload", uploadController);
 // POST with code snippet and sample input and ...
 // ... expected output data (no file upload)
 router.post("/submit", paramValidator, submitController);
