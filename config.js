8fae9bffdff252540da78ee712613468a432cb53
Roshan <nahsorad@gmail.com>
2020-10-22 17:17:33 +0545

    Export client's protocol, host, port, and redis' host, port, and constraints

--- config.js
+++ config.js
@@ -4,8 +4,12 @@ if (!process.env) throw new Error("Environment variable(s) not set.");
 
 module.exports = {
 	PORT: process.env.PORT || 8080,
-	LIVE_SERVER_PORT: process.env.LIVE_SERVER_PORT,
-	SECRET_DIVIDER_TOKEN: process.env.SECRET_DIVIDER_TOKEN,
+	CLIENT_PROTOCOL: process.env.CLIENT_PROTOCOL,
+	CLIENT_HOST: process.env.CLIENT_HOST,
+	CLIENT_PORT: process.env.CLIENT_PORT,
 	SECRET_SESSION_KEY: process.env.SECRET_SESSION_KEY,
-	REDIS_PORT: process.env.REDIS_PORT,
+	REDIS_STORE_HOST: process.env.REDIS_STORE_HOST,
+	REDIS_STORE_PORT: process.env.REDIS_STORE_PORT,
+	EXECUTION_TIME_OUT_IN_MS: process.env.EXECUTION_TIME_OUT_IN_MS,
+	MAX_LENGTH_STDOUT: process.env.MAX_LENGTH_STDOUT,
 };
