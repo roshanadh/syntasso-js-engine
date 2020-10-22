38d5f579c2d23f89300b719162ab66c20ae6b562
Roshan <nahsorad@gmail.com>
2020-10-22 17:55:50 +0545

    Refactor cleanup tests

--- test/7-cleanup.test.js
+++ test/7-cleanup.test.js
@@ -1,46 +1,33 @@
-const {
-	mocha,
-	chai,
-	should,
-	expect,
-	path,
-	fs,
-	execSync,
-} = require("./test-config.js");
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
 
-describe("5. Clean up files after socket disconnect", () => {
-	let socket, socketId, tempFilesPath, outputFilePath;
+const { mocha, chai, should, expect } = require("./test-config.js");
+
+describe("Test socket disconnection and cleanup:", () => {
+	let socket, socketId, clientFilesSubDirPath;
 	before(async () => {
 		const { removeConnection } = require("./test-config.js");
 		let removedConnection = await removeConnection();
 		socket = removedConnection.socket;
 		socketId = removedConnection.socketId;
-		tempFilesPath = path.resolve("client-files", socketId);
-		outputFilePath = path.resolve(
-			"client-files",
-			"outputs",
-			`${socketId}.txt`
-		);
+		clientFilesSubDirPath = path.resolve("client-files", socketId);
 	});
 
 	it("should disconnect from socket", done => {
 		expect(socket.connected).to.be.false;
 		done();
 	});
+
 	it("should remove the docker container if it was created", done => {
 		let filter = `\"name=${socketId}\"`;
-		const searchContainerOutput = execSync(`docker ps -aqf ${filter}`, {
-			stdio: ["pipe", "pipe", "pipe"],
-		});
+		const searchContainerOutput = execSync(`docker ps -aqf ${filter}`);
 		expect(searchContainerOutput.toString().trim()).to.be.empty;
 		done();
 	});
-	it("should remove the temporary client files if they were created", done => {
-		expect(fs.existsSync(tempFilesPath)).to.be.false;
-		done();
-	});
-	it("should remove the output file if it was created", done => {
-		expect(fs.existsSync(outputFilePath)).to.be.false;
+
+	it("should remove the client-files/{socketId} sub-directory if it was created", done => {
+		expect(fs.existsSync(clientFilesSubDirPath)).to.be.false;
 		done();
 	});
 });
