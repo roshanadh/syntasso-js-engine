9ffce5c03afc8ca1384fde8b2346891679c43791
Roshan <nahsorad@gmail.com>
2021-03-18 10:48:29 +0545

    Replace deprecated bodyParser with express middlewares

--- server.js
+++ server.js
@@ -78,8 +78,8 @@ if (cluster.isMaster && !module.parent) {
 
 	app.set("json spaces", 2);
 
-	app.use(bodyParser.urlencoded({ extended: false }));
-	app.use(bodyParser.json());
+	app.use(express.urlencoded({ extended: false }));
+	app.use(express.json());
 
 	app.use(router);
 }
