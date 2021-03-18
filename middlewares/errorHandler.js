0dbe4ff7d752642318a418773464f925bfc153f7
Roshan <nahsorad@gmail.com>
2021-03-18 18:57:07 +0545

    Log errors using log4js logger

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,8 @@
+const logger = require("../util/logger.js");
+
 module.exports = (error, req, res, next) => {
 	// this is the error-handling middleware
-	console.error("Error caught by the error-handling middleware:", error);
+	logger.error("Error caught by the error-handling middleware:", error);
 	return res.status(error.status || 503).json({
 		error: "Service unavailable due to server conditions",
 	});
