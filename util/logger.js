6906bc1b33bf1f2e31479596386ca182f6d52e30
Roshan <nahsorad@gmail.com>
2021-03-18 12:28:57 +0545

    Create a singleton SharedLogger class

--- util/logger.js
+++ util/logger.js
@@ -1,5 +1,7 @@
 const log4js = require("log4js");
 
+const { NODE_ENV } = process.env;
+
 log4js.configure({
 	appenders: {
 		console: { type: "console" },
@@ -27,4 +29,17 @@ log4js.configure({
 	},
 });
 
-module.exports = log4js;
+class SharedLogger {
+	_logger = undefined;
+
+	static getSharedLogger() {
+		if (!this._logger) {
+			this._logger = log4js.getLogger(
+				NODE_ENV === "dev" ? "dev" : "default"
+			);
+		}
+		return this._logger;
+	}
+}
+
+module.exports = SharedLogger.getSharedLogger();
